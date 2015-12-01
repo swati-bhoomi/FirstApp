d3da6eb11018eb3abc20821be96dcc6d6a738257
vinayaki-bhoomi <vinayak@bhoominfosolutions.com>
2015-12-01 11:15:08 +0530

    Updated from vinayak-pc swati

--- FirstApp/ReportForm.Designer.cs
+++ FirstApp/ReportForm.Designer.cs
@@ -25,11 +25,32 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 272);
+            this.Controls.Add(this.button1);
+            this.Name = "ReportForm";
             this.Text = "ReportForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
